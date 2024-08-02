b7feb1792beaa00e2ec4f70b8b88e07ce26053b5
mohinder <mohindernarula@gmail.com>
2024-08-02 22:30:09 +0530

    try

---
+++ airline/users/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+def index(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+    return render(request, "users/users.html")
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username = username, password = password)
+        
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "users/login.html", {
+                "messege": "Invalid Credidentials"
+            })
+        
+    return render(request, "users/login.html")
+
+
+
+def logout_view(request):
+    logout(request)
+    return render(request, "users/login.html",{
+        "messege": "Logged Out"
+    })
+    
+
+# Create your views here.
